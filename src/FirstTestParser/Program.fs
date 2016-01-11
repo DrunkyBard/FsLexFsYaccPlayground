6e67ce4d20badfbfebc0901521cf32f619cbb59c
DrunkyBard <sattar.imamov@gmail.com>
2016-01-11 23:42:43 +0300

    - Define SqlQuery AST - Some SQL lexer implementation

--- src/FirstTestParser/Program.fs
+++ src/FirstTestParser/Program.fs
@@ -1,4 +1,10 @@
 ﻿[<EntryPoint>]
 let main argv = 
+    let sqlQuery = "
+        SELECT a.Id, b.Name
+        FROM Table1 as a
+        INNER JOIN Table2 b ON a.Id = b.Id
+        WHERE a.Name = \"ABC\" AND b.SomeCol = true
+    "
     printfn "%A" argv
     0 
---
+++ src/FirstTestParser/SqlQuery.fs
@@ -0,0 +1,36 @@
+﻿module SqlQuery
+
+type Column = 
+    { 
+        TableRef : string
+        ColumnName : string 
+    }
+
+type Op = Eq | Neq | Lt | Gt | Leq | Geq 
+
+type Value = 
+    | Int of int
+    | Bool of bool
+    | String of string
+
+type Expr = 
+    | RefOnly of Op * Column * Column
+    | RefValue of Op * Column * Value
+    | Value of Op * Value * Value
+
+type WhereClause = 
+    | Expr of Expr
+    | And of WhereClause * WhereClause
+    | Or of WhereClause * WhereClause
+
+type JoinType = INNER | LEFT | RIGHT
+
+type JoinClause = string * JoinType * WhereClause
+
+type SqlQuery = 
+    { 
+        Table: string
+        Columns: Column list
+        Joins: JoinClause list
+        Where: WhereClause option
+    }
