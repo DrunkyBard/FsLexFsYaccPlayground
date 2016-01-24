a86a898b8666ec4a18b264bb13d04b00e9eb96be
DrunkyBard <sattar.imamov@gmail.com>
2016-01-25 01:56:14 +0300

    - Add MathDotNet library - Partially implement TexInterpreter module

--- src/TexCalculator/Program.fs
+++ src/TexCalculator/Program.fs
@@ -1,10 +1,12 @@
 ﻿module Program
 open TexParser
+open TexInterpreter
 open TexAst
 open DsLex
 open DsParser
 open Microsoft.FSharp.Text.Lexing
 open ParserExtensions
+open System.Linq.Expressions
 
 let readLexems lexbuf =
     let rec readLexemsInternal state = function
@@ -35,13 +37,20 @@ let main argv =
 //    let stringFormula = "1*2+3*4"
 //    let stringFormula = "{1}^{{2}^{3}}"
 //    let stringFormula = "1*2+5* {\\frac{\int_{[||for human select voice_freq where name=\"Lex\" and gender=1 or name=\"Parse\" and gender = 2||]}^{\pi} {x} d{x}}{((4 + \sum{1+2!, 3!+4})*3)}}^{3!} * 5"
-    let stringFormula = "[|for human select voice_freq where name=\"Lex\" and gender=1 or name=\"Parse\" and gender = 2|]"
+//    let stringFormula = "[|for human select voice_freq where name=\"Lex\" and gender=1 or name=\"Parse\" and gender = 2|]"
 //    let stringFormula = "\\frac{\int_{0}^{\pi} {x} d{x}}{((4 + \sum{1+2!, 3!+4})*3)}"
 //    let stringFormula = "2+3/4"
+//    let stringFormula = "1*2+\sum_{i}^{N}{N*\\text{[||for human select voice_freq where name=\"Lex\" and gender=1 or name=\"Parse\" and gender = 2||]}, 2, 5*2}"
+//    let stringFormula = "1+2"
+    let stringFormula = "\int_{0}^{10}{x+10} d{x}"
     let lexBuf = LexBuffer<char>.FromString stringFormula
 //    let lexems = readLexems  lexBuf 
-
+//    let par = Expression.Parameter(typeof<int>, "x")
+//    let lam = Expression.Lambda<System.Func<int, int>>(
+//        Expression.Add(par, Expression.Constant(2)), par).Compile()
+//    let w = lam.Invoke(5)
     let ast = (DsParserExtensions.parse, lexBuf) ||> TexParser.parse
+    let res = TexInterpreter.execute ast
     printfn "%A" ast
     0
 
--- src/TexCalculator/TexInterpreter.fs
+++ src/TexCalculator/TexInterpreter.fs
@@ -1,6 +1,52 @@
 ﻿module TexInterpreter
 
+open System
 open TexAst
+open MathNet.Numerics
+open System.Linq.Expressions
 
-let execute (ast: TexAst.Expr) : int = 
-    1
+let rec execute (ast: TexAst.Expr) : float = 
+    match ast with
+        | Plus(e1, e2) -> execute(e1) + execute(e2)
+        | Mul (e1, e2) -> execute(e1) * execute(e2)
+        | Sub (e1, e2) -> execute(e1) - execute(e2)
+        | Div (e1, e2) -> execute(e1) / execute(e2)
+        | Pow (e1, e2) -> System.Math.Pow(float(execute(e1)), float(execute(e2)))
+        | Int(x) -> float(x)
+        | Constant(Pi) -> System.Math.PI
+        | Constant(E) -> System.Math.E
+        | Neg(e) -> -1. * execute(e)
+        | Integral(func, diffVar, lowBE, uppBE) -> 
+            let diffParameter = Expression.Parameter(typeof<float>, diffVar)
+            let expr = buildExpression diffParameter func
+            let lam = Expression.Lambda<Func<float, float>>(expr, diffParameter)
+            let lamFunc = lam.Compile()
+            Integrate.OnClosedInterval(lamFunc, execute(lowBE), execute(uppBE))
+//        | Sum(idxId, upperBoundId, exprs) -> 
+//            match upperBoundId with
+//                | Some(id)
+//            let evalExprs = List.map execute exprs
+
+and buildExpression lamParameter expr : Expression = 
+    match expr with
+        | Plus(e1, e2) -> 
+            let v1 = buildExpression lamParameter e1
+            let v2 = buildExpression lamParameter e2
+            Expression.Add(v1, v2) :> Expression
+        | Sub(e1, e2) -> 
+            let v1 = buildExpression lamParameter e1
+            let v2 = buildExpression lamParameter e2
+            Expression.Subtract(v1, v2) :> Expression
+        | Mul(e1, e2) -> 
+            let v1 = buildExpression lamParameter e1
+            let v2 = buildExpression lamParameter e2
+            Expression.Multiply(v1, v2) :> Expression
+        | Div(e1, e2) -> 
+            let v1 = buildExpression lamParameter e1
+            let v2 = buildExpression lamParameter e2
+            Expression.Divide(v1, v2) :> Expression
+        | Var(x) -> 
+            if x <> lamParameter.Name then failwithf "Incorrect parameter. Expected: '%s'. Actual: '%s'" lamParameter.Name x
+            else lamParameter :> Expression
+        | expr -> Expression.Constant(execute expr) :> Expression
+
