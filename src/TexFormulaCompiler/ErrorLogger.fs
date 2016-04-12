5c1c5e83ae9b746f17be5746a4a404dff084aa51
DrunkyBard <sattar.imamov@gmail.com>
2016-04-12 17:30:40 +0300

    - Add ErrorLogger type - Add Error recovery case for binaryExpression production

---
+++ src/TexFormulaCompiler/ErrorLogger.fs
@@ -0,0 +1,11 @@
+﻿namespace ErrorHandling
+
+type Position = { Line: int; Column: int }
+
+type ParseError = Error of string * Position
+
+type ErrorLogger(out : string -> unit) = 
+    member this.ReportError (Error(message, pos)) = 
+        let pos2 : Position = {Line = 2; Column = 3}
+        let errorMsg = sprintf "%s at Line: %d, Column: %d" message pos.Line pos.Column
+        out(errorMsg)
--- src/TexFormulaCompiler/Program.fs
+++ src/TexFormulaCompiler/Program.fs
@@ -8,6 +8,8 @@ open Compiler
 open DsParserExtensions
 open System.Linq.Expressions
 open FSharp.Quotations.Evaluator
+open Microsoft.FSharp.Text.Lexing
+open Microsoft.FSharp.Text.Parsing
 
 
 let readLexems lexbuf =
@@ -26,8 +28,13 @@ let readLexems1 single lexbuf =
 
 [<EntryPoint>]
 let main argv = 
+    let parseState : IParseState = Unchecked.defaultof<IParseState>
+    let lexbuf = parseState.ParserLocalStore.["LexBuffer"] :?> LexBuffer<char>
     let stringFormula = "1*2+5* {\\frac{\int_{[|for A with Id = \"A2\" select IntProp|]}^{\pi} {x} d{x}}{((4 + \sum{1+2!, 3!+4})*3)}}^{3!} * 5"
     let lexbuf = LexBuffer<char>.FromString stringFormula
+//    lexbuf.StartPos.Line
+//    lexbuf.StartPos.Column
+//    lexbuf.Lexeme.ToString()
     let lexems = readLexems lexbuf
     let compiler = TexCompiler(DsParser.parse, DomainSpecificInterpreter.evalSValue, DomainSpecificInterpreter.evalMValue)
     let intermediateExpression = 
